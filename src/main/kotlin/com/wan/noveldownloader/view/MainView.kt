97f9dab4e87ce47e5b3c7f882fff92f7f2519827
Stars-One <Stars-One@users.noreply.github.com>
2019-12-22 16:40:14 +0800

    清理多余的代码

--- src/main/kotlin/com/wan/noveldownloader/view/MainView.kt
+++ src/main/kotlin/com/wan/noveldownloader/view/MainView.kt
@@ -1,15 +1,11 @@
 package com.wan.noveldownloader.view
 
 import com.wan.noveldownloader.app.Styles
-import javafx.scene.image.ImageView
 import kfoenix.jfxtabpane
 import tornadofx.*
-import java.io.File
-import java.net.URL
 
 class MainView : View("星之小说下载器 by stars-one") {
-
-    private var image by singleAssign<ImageView>()
+    
     override val root = hbox {
         val downloadedView = DownloadedView()
         setPrefSize(800.0, 500.0)
@@ -36,21 +32,5 @@ class MainView : View("星之小说下载器 by stars-one") {
         }
     }
 
-    fun downloadImage(imgUrl: String): URL {
-        //jar包当前目录
-        val dir = File(".", "星之小说下载器${File.separator}img")
-        if (!dir.exists()) {
-            dir.mkdirs()
-        }
-        //缓存文件名处理
-        val fileName = imgUrl.substringAfterLast("/")
-        val file = File(dir, fileName)
-        if (!file.exists()) {
-            val conn = URL(imgUrl).openConnection()
-            conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)")
-            val bytes = conn.getInputStream().readBytes()
-            file.writeBytes(bytes)
-        }
-        return file.toURI().toURL()
-    }
+
 }
